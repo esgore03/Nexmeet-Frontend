bee9662f756f05a78566b3006d6946c98073b26a
esgore03 <esteban.gomez.reyes@correounivalle.edu.do>
2025-11-17 10:03:22 -0500

    constants changes and navbar deletion

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@
  * Replace this value with your backend URL if needed.
  * @constant {string}
  */
-export const API_BASE_URL = "https://popfix-back.onrender.com/api";
+export const API_BASE_URL = "";
 
 /**
  * API endpoint definitions for different functionalities.
@@ -15,36 +15,14 @@ export const API_BASE_URL = "https://popfix-back.onrender.com/api";
  * @property {string} REGISTER - Endpoint for user registration.
  * @property {string} FORGOT_PASSWORD - Endpoint for password recovery.
  * @property {string} LOGOUT - Endpoint for logging out the user.
- * @property {string} MOVIES - Endpoint for listing or managing movies.
- * @property {string} MOVIE_DETAILS - Endpoint for fetching movie details by ID.
- * @property {string} SEARCH_MOVIES - Endpoint for searching movies.
- * @property {string} POPULAR_MOVIES - Endpoint for fetching popular movies.
- * @property {string} TRENDING_MOVIES - Endpoint for fetching trending movies.
- * @property {string} USER_PROFILE - Endpoint for getting user profile information.
- * @property {string} USER_FAVORITES - Endpoint for getting user's favorite movies.
+
  */
 export const API_ENDPOINTS = {
   // Autenticación
-  LOGIN: '/users/login',
-  REGISTER: '/users/register',
-  FORGOT_PASSWORD: '/auth/forgot-password',
-  LOGOUT: '/logout',
-  
-  // Películas
-  MOVIES: '/movies/mixed',
-  MOVIE_DETAILS: '/pexels/:id',
-  SEARCH_MOVIES: '/pexels/search',
-  POPULAR_MOVIES: '/pexels/popular',
-  TRENDING_MOVIES: '/pexels/trending',
-  
-  // Favoritos y ratings
-  MOVIES_FAVORITES: '/movies/favorites/:userId',
-  MOVIES_INSERT_FAVORITE_RATING: '/movies/insertFavoriteRating/:userId',
-  MOVIES_UPDATE: '/movies/update/:userId',
-  
-  // Usuario
-  USER_PROFILE: '/user/profile',
-  USER_FAVORITES: '/user/favorites'
+  LOGIN: "/users/login",
+  REGISTER: "/users/register",
+  FORGOT_PASSWORD: "/auth/forgot-password",
+  LOGOUT: "/logout",
 };
 
 /**
@@ -53,12 +31,8 @@ export const API_ENDPOINTS = {
  * @type {Object}
  * @property {string} APP_NAME - Name of the application.
  * @property {string} DEFAULT_LANGUAGE - Default language of the app (ISO code).
- * @property {number} MOVIES_PER_PAGE - Number of movies to display per page.
- * @property {string} IMAGE_BASE_URL - Base URL for movie poster images.
  */
 export const APP_CONFIG = {
-  APP_NAME: "PopFix",
+  APP_NAME: "Nexmeet",
   DEFAULT_LANGUAGE: "es",
-  MOVIES_PER_PAGE: 20,
-  IMAGE_BASE_URL: "https://image.tmdb.org/t/p/w500", // For movie images
 };
